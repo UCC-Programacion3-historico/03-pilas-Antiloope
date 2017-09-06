e74874de75a6478548d0eeb477cd4ff43cbca14f
Antiloope <rodrigopizarro1234@gmail.com>
2017-09-06 11:51:31 -0300

    Se completo el ejercicio 1

--- Ej-01/main.cpp
+++ Ej-01/main.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 #include "../Pila/Pila.h"
+#include <windows.h>
+#include <stdio.h>
+
+using namespace std;
 
 int main() {
-    std::cout << "Ejercicio 03/01\n" << std::endl;
+    char a[50];
+    cout << "Ingrese una palabra: " << '\n';
+    cin >> a;
+    Pila<char> inversa;
+    for (int i = 0; i < strlen(a); i++) {
+        inversa.push(a[i]);
+    }
+    while (!inversa.esVacia()) {
+        cout << inversa.pop() ;
+    }
     return 0;
 }
